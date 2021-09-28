79e7d9ff82e356a0f81f9e7d9baa4f5f16db1717
Jam Jam James Perey <jamjam.perey@gmail.com>
2021-09-28 00:20:20 -0700

    added components to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import Navbar1 from './components/Navbar1/Navbar1';
+import { React } from 'react';
 import './App.css';
+
+//components
+import Navbar1 from './components/Navbar1/Navbar1';
+import Navbar2 from './components/Navbar2/Navbar2';
+import carousel1 from './components/Carousel1/Carousel1';
+import carousel2 from './components/Carousel2/Carousel2';
+import carousel3 from './components/Carousel3/Carousel3';
+
+
 
 function App() {
   return (
     <div className= "App">
       <h1>Steam Copy Page</h1>
+
 
 
     </div>   
